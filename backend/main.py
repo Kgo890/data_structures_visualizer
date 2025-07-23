39fd3ab2ab4a0140fe29f287e2ec4e7e9996cbf8
Kgo890 <gorekaden501@gmail.com>
2025-07-23 14:58:38 -0700

    more new fixes

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://data-structures-visualizer-six.vercel.app/"],
+    allow_origins=["http://localhost:5173", "https://data-structures-visualizer-six.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
