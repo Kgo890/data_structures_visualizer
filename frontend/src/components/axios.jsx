c7a90f372a89c801648d0da677ac6e47b810bb14
Kgo890 <gorekaden501@gmail.com>
2025-07-23 15:02:58 -0700

    fixed axios.jsx

--- frontend/src/components/axios.jsx
+++ frontend/src/components/axios.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_REACT_APP_API || "http://localhost:8000";
 
-const BASE_URL = process.env.REACT_APP_API || "https://localhost:8000";
+console.log("API Base URL:", BASE_URL);
+
 const api = axios.create({
   baseURL: BASE_URL,
   headers: {
